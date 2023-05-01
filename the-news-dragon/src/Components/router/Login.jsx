2140606570c6e07c509a00c221d87a665a4df9be
Md Mahbub Alom <mdmahbuba034@gmail.com>
2023-05-01 14:57:55 +0600

    register form done

--- the-news-dragon/src/Components/router/Login.jsx
+++ the-news-dragon/src/Components/router/Login.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <Container className='w-25 mx-auto mt-5'>
+        <Container className='w-25 mx-auto mt-5 border rounded p-5'>
             <Form>
                 <h2>Please Login!!!</h2>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+                <Button className='mb-2' variant="primary" type="submit">
+                    Login
                 </Button>
+                <br />
+                <Form.Text className=" text-secondary">
+                    Don't have an Account? <Link to='/register'>Register</Link>
+                </Form.Text>
                 <Form.Text className="text-danger">
 
                 </Form.Text>
--- the-news-dragon/src/Components/router/Register.jsx
+++ the-news-dragon/src/Components/router/Register.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>this is register</h2>
-        </div>
+        <Container className='w-25 mx-auto mt-5 border rounded p-5'>
+            <Form>
+                <h2>Register your Account!!!</h2>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter Name" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Photo URL</Form.Label>
+                    <Form.Control type="text" name='photo' placeholder="Enter Your Photo URL" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" name='accept' label="Accept term & conditions" />
+                </Form.Group>
+                <Button className='mb-2' variant="primary" type="submit">
+                    Register
+                </Button>
+                <br />
+                <Form.Text className=" text-secondary">
+                    Already have an account? <Link to='/login'>Login</Link>
+                </Form.Text>
+                <Form.Text className="text-danger">
+
+                </Form.Text>
+                <Form.Text className="text-success">
+
+                </Form.Text>
+            </Form>
+        </Container>
     );
 };
 
