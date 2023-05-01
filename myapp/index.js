458ced14e6577de2db4f1b413c2a168f69b86322
Md Mahbub Alom <mdmahbuba034@gmail.com>
2023-05-01 14:32:50 +0600

    hudai commit dilam

--- myapp/index.js
+++ myapp/index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 
-const categories = require('./categories.json')
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.use(cors())
 
@@ -13,6 +14,25 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(newses => newses._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(newses => parseInt(newses.category_id) === id);
+        res.send(categoryNews)
+    }
 })
 
 app.listen(port, () => {
